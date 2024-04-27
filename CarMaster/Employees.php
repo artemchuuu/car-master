31bc48c8279e3c6a097a348c912ebbbd452bd496
Chev20 <chelioschev2020@gmail.com>
2024-04-28 01:51:05 +0300

    changed getCarStatus and code editing

--- CarMaster/Employees.php
+++ CarMaster/Employees.php
@@ -13,58 +13,37 @@ class Employees
     protected int $age;
     protected float $salary;
 
-    /**
-     * @return void
-     */
     public function startOfTheRepair(): void
     {
         echo "*Починається робота*\n";
     }
 
-    /**
-     * @param string $name
-     */
     public function setName(string $name): void
     {
         $this->validName($name);
         $this->name = $name;
     }
 
-    /**
-     * @return string
-     */
     public function getName(): string
     {
         return $this->name;
     }
 
-    /**
-     * @param int $age
-     */
     public function setAge(int $age): void
     {
         $this->age = $age;
     }
 
-    /**
-     * @return int
-     */
     public function getAge(): int
     {
         return $this->age;
     }
 
-    /**
-     * @param float $salary
-     */
     public function setSalary(float $salary): void
     {
         $this->salary = $salary;
     }
 
-    /**
-     * @return float
-     */
     public function getSalary(): float
     {
         return $this->salary;
